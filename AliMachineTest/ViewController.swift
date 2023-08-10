43e4ecf8ee0c0c5fcd80c93164174b68e26b44d7
hemant <hemantrajput825@gmail.com>
2023-08-10 13:21:19 +0530

    Initial Commit

---
+++ AliMachineTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AliMachineTest
+//
+//  Created by Hemant Singh Rajput on 10/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
